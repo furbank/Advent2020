898fa7f7d9bac75c5ddc65f643786321ac6671c7
Stephen Furbank <furbank@hotmail.com>
2020-12-04 12:14:12 +0000

    cleaned up

--- Day_01/code.py
+++ Day_01/code.py
@@ -2,12 +2,8 @@
 with open("Day_01\input.txt") as f:
     content = [int(i) for i in f.read().splitlines()]
 
-f.close() #not sure this is needed following the "with"
-
 while content:
     a = content.pop()
     r = [x * a for x in content if (x+a==2020)]
     if r: print(r.pop())
-
-#This only works because the data set only has one correct answer
 
