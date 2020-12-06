40d7387d90125371f418b4a0113a830bf8663f98
Stephen Furbank <furbank@hotmail.com>
2020-12-06 21:17:35 +0000

    working

--- Day_02/part1.py
+++ Day_02/part1.py
@@ -1,4 +1,17 @@
 with open("Day_02\input.txt") as f:
     content = f.read().splitlines()
 
-print(content)
+def check_pw( line ):
+    rule_min = (line.split())[0].split('-')[0]
+    rule_max = (line.split())[0].split('-')[1]
+    rule = (line.split())[1][:1]
+    pw = (line.split())[2]
+
+    return int(rule_min) <= pw.count(rule) <= int(rule_max)
+
+n=0
+for x in content:
+    if check_pw(x): n += 1
+
+print(n)
+
