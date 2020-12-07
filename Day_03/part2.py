d394a52070337b270a5b55946544a19f5bfd7692
Stephen Furbank <furbank@hotmail.com>
2020-12-07 12:57:37 +0000

    part2 working

--- Day_03/part2.py
+++ Day_03/part2.py
@@ -1,19 +1,21 @@
 with open("Day_03\input.txt") as f:
     content = f.read().splitlines()
 
-position = 0
-trees = 0
+def find_trees_in_path( move_right, move_down):
+    position = 0
+    trees = 0
 
-for c in content:
-    if (c[position:position+1]) == '#':
-        trees += 1
-        tree = 'X'
-    else:
-        tree = 'O'
+    for c in content[::move_down]:
+        if (c[position:position+1]) == '#': trees += 1
+        position += move_right
+        if position >= len(c): position -= len(c)
 
-    c = c[:position] + tree + c[position+1:]
-    position += 3
-    if position >= len(c): position -= len(c)
-    print(c)
+    return(trees)
 
-print(trees)
+num = find_trees_in_path( 1, 1)
+num *= find_trees_in_path( 3, 1)
+num *= find_trees_in_path( 5, 1)
+num *= find_trees_in_path( 7, 1)
+num *= find_trees_in_path( 1, 2)
+
+print(num)
