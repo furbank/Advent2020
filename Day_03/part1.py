aa4e48f33159dade42755366a2fc34827a0f308a
Stephen Furbank <furbank@hotmail.com>
2020-12-07 12:10:31 +0000

    part 1 working

--- Day_03/part1.py
+++ Day_03/part1.py
@@ -1,7 +1,19 @@
 with open("Day_03\input.txt") as f:
     content = f.read().splitlines()
 
+position = 0
+trees = 0
+
 for c in content:
+    if (c[position:position+1]) == '#':
+        trees += 1
+        tree = 'X'
+    else:
+        tree = 'O'
+
+    c = c[:position] + tree + c[position+1:]
+    position += 3
+    if position >= len(c): position -= len(c)
     print(c)
 
-print(len(content))
+print(trees)
