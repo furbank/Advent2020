b21ca7dc5dbe83bc0dae78e2d5f1179fb2c5d959
Stephen Furbank <furbank@hotmail.com>
2020-12-07 21:30:32 +0000

    removed dev code

--- Day_04/part1.py
+++ Day_04/part1.py
@@ -12,14 +12,7 @@ data = [c.split(' ') for c in data]
 data = [[kv.split(':')[0] for kv in passport if kv.split(':')[0] != 'cid'] for passport in data]
 
 # get count of lists with 7 items
-
 print(len([keys for keys in data if len(keys) == 7]))
 
-
-
-# test = ['iyr:2013', 'hcl:#ceb3a1', 'hgt:151cm', 'eyr:2030', 'byr:1943', 'ecl:grn']
-# test = [(t.split(':')[0]) for t in test if t.split(':')[0] != 'hcl']
-# #test.sort()
-# print(test)
 
 
