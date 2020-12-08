edcb46d3266f67f98406ced86511b83b82822c28
Stephen Furbank <furbank@hotmail.com>
2020-12-08 17:47:47 +0000

    Day 6 part 2 working

--- Day_06/part2.py
+++ Day_06/part2.py
@@ -1,7 +1,12 @@
+from collections import Counter
+
 with open("Day_06\input") as f:
     data = f.read().split('\n\n')
 
 # check for and remove trailing whitespace
 data[-1] = data[-1].rstrip()
+data = [d.replace('\n','-') + '-' for d in data]
+data = [Counter(d) for d in data]
 
-print(sum([len(set(d.replace('\n',''))) for d in data]))
+data = [len([x for x in d.items() if x[1] == d['-'] and x[0] != '-']) for d in data]
+print(sum(data))
