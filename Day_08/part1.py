a422fb84149ca86c7413eec39230653065edf779
Stephen Furbank <furbank@hotmail.com>
2020-12-10 12:32:04 +0000

    removed enumerate wasn't used

--- Day_08/part1.py
+++ Day_08/part1.py
@@ -1,18 +1,7 @@
 with open("Day_08\input") as f:
     data = f.read().splitlines()
 
-# data = ['nop +0',
-# 'acc +1',
-# 'jmp +4',
-# 'acc +3',
-# 'jmp -3',
-# 'acc -99',
-# 'acc +1',
-# 'jmp -4',
-# 'acc +6']
-
-data = list(enumerate(data))
-bootInst = [[d[0], d[1][:3], int(d[1][4:])] for d in data]
+bootInst = [[d[:3], int(d[4:])] for d in data]
 
 def acc(x):
     global acc, currentInst
@@ -35,6 +24,6 @@ execInst = set()
 
 while currentInst not in execInst and currentInst < len(bootInst):
     execInst.add(currentInst)
-    inst[bootInst[currentInst][1]](bootInst[currentInst][2])
+    inst[bootInst[currentInst][0]](bootInst[currentInst][1])
 
 print(acc)
