00af82cbc8b3b4036a27c88cf5a1601eb1aa020b
Stephen Furbank <furbank@hotmail.com>
2020-12-10 10:43:52 +0000

    day 7 part 2 finally working

--- Day_07/part2.py
+++ Day_07/part2.py
@@ -1,4 +1,25 @@
 with open("Day_07\input") as f:
-    rules = f.read().splitlines()
+    data = f.read().splitlines()
 
+# clean data
+data = [d.split(' bags contain ') for d in data]
+#data = [[d[0], '0 ' + d[1][2:] if 'no other' in d[1] else d[1]] for d in
+data = [[d[0], d[1].replace('no other', '0 other')] for d in data]
+data = [[d[0], d[1][:-1].replace(' bags', '').replace(' bag', '').split(', ')] for d in data]
+
+# expand rules to 1 per list element
+rules = []
+[[rules.append([d[0], int(r[0]), r[2:]]) for r in d[1]] for d in data]
+
+totalBags = 0
 myBag = 'shiny gold'
+bags = [r for r in rules if r[0] == myBag]
+
+while bags:
+    b = bags.pop()
+    totalBags += b[1]
+
+    for r in [r for r in rules if r[0] == b[-1]]:
+        if r[1] !=0: bags.append([r[0], (b[1]*r[1]), r[2]])
+
+print(totalBags)
