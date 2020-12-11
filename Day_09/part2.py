f8454f529caa1fe24a21f0708c85988d7c1cef45
Stephen Furbank <furbank@hotmail.com>
2020-12-11 16:55:38 +0000

    Day 9 finished

--- Day_09/part2.py
+++ Day_09/part2.py
@@ -1,14 +1,24 @@
 with open("Day_09\input") as f:
     data = f.read().splitlines()
+
 data = list(enumerate([int(d) for d in data]))
 
 preamble = 25
 
 for d in [d for d in data[preamble:]]:
-    offset = d[0]
-    check = d[1]
-    candidates = (set([a[1] +b[1] for b in data[offset-preamble:offset] for a in data[offset-preamble:offset] if a[0] != b[0]]))
-
-    if check not in candidates:
-        print('First no match:', d[1], 'at position:', d[0])
+    if d[1] not in (set([a[1] + b[1] for b in data[d[0]-preamble:d[0]] for a in data[d[0]-preamble:d[0]] if a[0] != b[0]])):
+        noMatch = d[1]
+        noMatchPos = d[0]
         break
+
+print('First no match:', noMatch, 'at input line:', noMatchPos + 1)
+
+for x in data:
+    for y in data[x[0]+2:]:
+        checkRange = [d[1] for d in data[x[0]:y[0]]]
+        if sum(checkRange) >= noMatch:
+            break
+    if sum(checkRange) == noMatch:
+        break
+
+print('Encryption weakness:', min(checkRange) + max(checkRange))
