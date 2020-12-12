670ee84dfd545bc58fb38a3bad903c148db6143f
Stephen Furbank <furbank@hotmail.com>
2020-12-12 16:03:25 +0000

    day 10 part 1 working

--- Day_10/part1.py
+++ Day_10/part1.py
@@ -1,4 +1,11 @@
 with open("Day_10\input") as f:
     data = f.read().splitlines()
 
-print(data)
+data = [int(d) for d in data]
+data.append(0)
+data.append(max(data)+ 3)
+data.sort()
+data = list(enumerate(data))
+differences = [b[1]-a[1] for b in data for a in data if b[0] == a[0]+1]
+
+print(len([d for d in differences if d == 1]) * len([d for d in differences if d == 3]))
