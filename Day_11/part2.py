35668fe1e8c0850e9203208d0e71239fb0685d0c
Stephen Furbank <furbank@hotmail.com>
2020-12-18 15:38:52 +0000

    Working but slow

--- Day_11/part2.py
+++ Day_11/part2.py
@@ -1,16 +1,12 @@
 with open("Day_11\input") as f:
     data = f.read().splitlines()
 
-def IsSeatGood(row, col, maxOcc, plan):
+def OccupiedNeighbours(row, col, plan):
     import numpy as np
 
     plan = np.array([[col for col in row] for row in plan])
 
-    occupied = 0
-    lenCol = len(plan[0,:])
-    lenRow = len(plan[:,0])
-    maxCol = lenCol -1
-    maxRow = lenRow -1
+    maxCol = len(plan[0,:]) -1
 
     #Slice line of sight out of plan
     #Horizontal
@@ -24,8 +20,9 @@ def IsSeatGood(row, col, maxOcc, plan):
     losLD = plan.diagonal(lOffset)
 
     #Right diagonal
-    rOffset = (lenRow - col) - row
+    rOffset = (maxCol - col) -row
     losRD = np.fliplr(plan).diagonal(rOffset)
+
 
     los = []
     #flip start slice so they can all be processed the same
@@ -33,35 +30,45 @@ def IsSeatGood(row, col, maxOcc, plan):
     los.append(list(losH[col+1:]))
     los.append(list(np.flip(losV[:row])))
     los.append(list(losV[row+1:]))
-    los.append(list(np.flip(losLD[:col])))
-    los.append(list(losLD[col+1:]))
-    los.append(list(np.flip(losRD[:row])))
-    los.append(list(losRD[row+1:]))
+    if lOffset < 0:
+        los.append(list(np.flip(losLD[:row+lOffset])))
+        los.append(list(losLD[row+1+lOffset:]))
+    else:
+        los.append(list(np.flip(losLD[:row])))
+        los.append(list(losLD[row+1:]))
+
+    if rOffset < 0:
+        los.append(list(np.flip(losRD[:row+rOffset])))
+        los.append(list(losRD[row+1+rOffset:]))
+    else:
+        los.append(list(np.flip(losRD[:row])))
+        los.append(list(losRD[row+1:]))
 
     vis = [[f for f in l if f != '.'] for l in los if l != []]
-    return ([v[0] for v in vis if v != []].count('#')) < maxOcc
+    return ([v[0] for v in vis if v != []].count('#'))
 
-print(IsSeatGood(10, 96, 5, data))
+maxNeighbours = 5
+plan = data.copy()
+oldPlan = data.copy()
 
-# plan = data.copy()
-# oldPlan = data.copy()
+for i in range(1000):
 
-# for i in range(100):
+    for r in range(len(data)):
+        for c in range(len(data[r])):
+            # if c ==9 and r == 7:
+            #     print('break')
+            if oldPlan[r][c] == 'L' and OccupiedNeighbours(r, c, oldPlan) == 0:
+                # Sit Here
+                plan[r] = plan[r][:c] + '#' + plan[r][c+1:]
 
-    # for r in range(len(oldPlan)):
-        # for c in range(len(oldPlan[r])):
-            # if oldPlan[r][c] == 'L' and '#' not in GetAdjacent(r, c, oldPlan):
-                # # Sit Here
-                # plan[r] = plan[r][:c] + '#' + plan[r][c+1:]
+            if oldPlan[r][c] == '#' and OccupiedNeighbours(r, c, oldPlan) >= maxNeighbours:
+                # Vacate Here
+                plan[r] = plan[r][:c] + 'L' + plan[r][c+1:]
 
-            # if oldPlan[r][c] == '#' and GetAdjacent(r, c, oldPlan).count('#') > 3:
-                # # Vacate Here
-                # plan[r] = plan[r][:c] + 'L' + plan[r][c+1:]
+    if oldPlan == plan:
+        print('Stabilized on iteration:', i)
+        break
 
-    # if oldPlan == plan:
-        # print('Stabilized on iteration:', i)
-        # break
+    oldPlan = plan.copy()
 
-    # oldPlan = plan.copy()
-
-# print('Occupied seats:',sum([p.count('#') for p in plan]))
+print('Occupied seats:',sum([p.count('#') for p in plan]))
