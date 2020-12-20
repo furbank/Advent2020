2745452d401cb37ba065ec49e9a8c43df749a492
Stephen Furbank <furbank@hotmail.com>
2020-12-20 17:50:02 +0000

    arrange data to show target state

--- Day_13/part2.py
+++ Day_13/part2.py
@@ -3,6 +3,11 @@ with open("Day_13\input") as f:
 
 #data = ['939', '7,13,x,x,59,x,31,19']
 
-timetable = [d for d in data[1].split(',')]
-[print(t) for t in timetable]
+target = {}
+offset = 0
 
+for d in data[1].split(','):
+    if d.isnumeric():
+        target[offset] = int(d)
+        print(f'{offset:>}: {d}')
+    offset += 1
