0853398ce8861009d2252d16962e7f689863806e
Stephen Furbank <furbank@hotmail.com>
2020-12-20 17:09:28 +0000

    Day 13 part 1 working

--- Day_13/part1.py
+++ Day_13/part1.py
@@ -1,11 +1,14 @@
 with open("Day_13\input") as f:
     data = f.read().splitlines()
 
-[print(d) for d in data]
+#data = ['939', '7,13,x,x,59,x,31,19']
 
-data = [
-    '939',
-    '7,13,x,x,59,x,31,19'
-]
+eta = int(data[0])
+timetable = [int(d) for d in data[1].split(',') if d !='x']
 
-[print(d) for d in data]
+arrivals = {t:t*(eta//t+1) for t in timetable}
+
+nextBusId = min(arrivals, key=arrivals.get)
+
+print('Solution:', nextBusId * (arrivals.get(nextBusId) - eta))
+
