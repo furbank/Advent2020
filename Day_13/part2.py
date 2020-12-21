6f4f8e029c137b99ad1865cb45900bca22f307cb
Stephen Furbank <furbank@hotmail.com>
2020-12-21 19:19:08 +0000

    Enter, stage left, Chinese Remainder Theorem

--- Day_13/part2.py
+++ Day_13/part2.py
@@ -1,13 +1,29 @@
+'''
+My knowledge of maths is not up to this, but some googling for a hint brought me to Chinese Remainder Theorem
+Explanation here: https://youtu.be/ru7mWZJlRQg
+
+Because I'm lazy and only just (maybe) understand this I found some code to "borrow"
+https://rosettacode.org/wiki/Chinese_remainder_theorem
+
+I'm going to take the time to understand this because it's the first python code of any length from examples I've
+used and there is probably a lot to learn in it.
+'''
 with open("Day_13\input") as f:
     data = f.read().splitlines()
 
-#data = ['939', '7,13,x,x,59,x,31,19']
 
-target = {}
+# data = ['939', '7,13,x,x,59,x,31,19']
+# data = ['x', '17,x,13,19']
+
+target = []
 offset = 0
 
 for d in data[1].split(','):
     if d.isnumeric():
-        target[offset] = int(d)
-        print(f'{offset:>}: {d}')
+        target.append( (offset, int(d)))
     offset += 1
+
+[print(t) for t in target]
+
+
+
