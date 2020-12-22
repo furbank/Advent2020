ba2443c3581feaecaac31f209744ce66eb0b19c3
Stephen Furbank <furbank@hotmail.com>
2020-12-22 18:00:40 +0000

    Working but lots i don't understand well

--- Day_13/part2.py
+++ Day_13/part2.py
@@ -1,29 +1,32 @@
-'''
-My knowledge of maths is not up to this, but some googling for a hint brought me to Chinese Remainder Theorem
-Explanation here: https://youtu.be/ru7mWZJlRQg
+# '''
+# My knowledge of maths is not up to this, but some googling for a hint brought me to Chinese Remainder Theorem
+# Explanation here: https://youtu.be/ru7mWZJlRQg
+#
+# Because I'm lazy and only just (maybe) understand this I found some code to "borrow"
+# https://rosettacode.org/wiki/Chinese_remainder_theorem
+#
+# Example: Find the smallest positive integer that can be divided by 3,5,7 to give remainders of 1,4,6 respectively.
+#
+# I'm going to take the time to understand this because it's the first python code of any length from examples I've
+# used and there is probably a lot to learn in it.
+# '''
+from chinese_remainder import chinese_remainder
 
-Because I'm lazy and only just (maybe) understand this I found some code to "borrow"
-https://rosettacode.org/wiki/Chinese_remainder_theorem
-
-I'm going to take the time to understand this because it's the first python code of any length from examples I've
-used and there is probably a lot to learn in it.
-'''
 with open("Day_13\input") as f:
     data = f.read().splitlines()
 
-
-# data = ['939', '7,13,x,x,59,x,31,19']
-# data = ['x', '17,x,13,19']
-
-target = []
-offset = 0
+n = []
+a = []
+pos = 0
 
 for d in data[1].split(','):
     if d.isnumeric():
-        target.append( (offset, int(d)))
-    offset += 1
+        n.append(int(d))
+        a.append(int(d) - pos)
+    pos += 1
 
-[print(t) for t in target]
+print(n)
+print(a)
+print()
 
-
-
+print(chinese_remainder(n, a))
