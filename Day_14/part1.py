20c6a39b43742346077bb4c29cc8e58bc2a37209
Stephen Furbank <furbank@hotmail.com>
2020-12-22 20:26:07 +0000

    parse input

--- Day_14/part1.py
+++ Day_14/part1.py
@@ -1,4 +1,12 @@
 with open("Day_14\input") as f:
     data = f.read().splitlines()
 
-[print(d) for d in data]
+mask = ''
+instructions = []
+for d in data:
+    if d[:4] == 'mask':
+        mask = d[7:]
+    else:
+        instructions.append([int(d[4:d.index(']')]), int(d[d.rindex(' '):]), mask])
+
+[print(i) for i in instructions]
