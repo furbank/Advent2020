0a1134910a34257e0dce512fefcc69537a1e7805
Stephen Furbank <furbank@hotmail.com>
2020-12-23 16:04:44 +0000

    Day 14 part 2 working

--- Day_14/part2.py
+++ Day_14/part2.py
@@ -1,4 +1,35 @@
 with open("Day_14\input") as f:
     data = f.read().splitlines()
 
-[print(d) for d in data]
+# Test data from problem
+# data = [
+#     'mask = 000000000000000000000000000000X1001X',
+#     'mem[42] = 100',
+#     'mask = 00000000000000000000000000000000X0XX',
+#     'mem[26] = 1',
+# ]
+
+# Parse input into list of instructions: Address, Value, Memory Address Decoder
+mad = ''
+instructions = []
+for d in data:
+    if d[:4] == 'mask':
+        mad = d[7:]
+    else:
+        instructions.append([int(d[4:d.index(']')]), int(d[d.rindex(' '):]), mad])
+
+# Expand instructions to full address space and write to memory
+PosibleBits = lambda m: [bin(f)[2:].zfill(m.count('X')) for f in range(2**m.count('X'))]
+RootAddress = lambda a, m: a & int(m.replace('0', '1').replace('X', '0'),2)
+
+mem = {} # Simulate memory as dictionary
+for i in instructions:
+    mask = ''
+    for pb in PosibleBits(i[2]):
+        mask = i[2]
+        [mask := mask.replace('X', b, 1) for b in pb]
+        mem[int(mask,2) | RootAddress(i[0], i[2])] = i[1]
+
+# Sum all remaining memory values
+print(sum(mem.values()))
+
