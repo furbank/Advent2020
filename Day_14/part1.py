5140e08b8b910c445ad9e4bb7d603037e7500d98
Stephen Furbank <furbank@hotmail.com>
2020-12-23 14:18:14 +0000

    Day 14 part 1 working

--- Day_14/part1.py
+++ Day_14/part1.py
@@ -1,12 +1,14 @@
 with open("Day_14\input") as f:
     data = f.read().splitlines()
 
+ApplyMask = lambda v, m: (v | int(m.replace('X', '0'),2)) & int(m.replace('X', '1'),2)
+
 mask = ''
-instructions = []
+instructions = {}
 for d in data:
     if d[:4] == 'mask':
         mask = d[7:]
     else:
-        instructions.append([int(d[4:d.index(']')]), int(d[d.rindex(' '):]), mask])
+        instructions[int(d[4:d.index(']')])] = ApplyMask( int(d[d.rindex(' '):]), mask)
 
-[print(i) for i in instructions]
+print(sum(instructions.values()))
